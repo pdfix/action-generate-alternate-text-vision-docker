aff76e07fb2825b99addbe876c560b6c0bd69034
Peter Kral <peter.kral@pdfix.net>
2025-06-03 13:45:13 +0200

    simplified download_models python script

--- download_models.py
+++ download_models.py
@@ -1,12 +1,5 @@
-import torch
+from transformers import AutoTokenizer, VisionEncoderDecoderModel, ViTImageProcessor
 
-from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
-
-
-model = VisionEncoderDecoderModel.from_pretrained(
-    "nlpconnect/vit-gpt2-image-captioning"
-)
-feature_extractor = ViTImageProcessor.from_pretrained(
-    "nlpconnect/vit-gpt2-image-captioning"
-)
+model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
