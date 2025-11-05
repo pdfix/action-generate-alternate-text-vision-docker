adc8d2fd75f76f8bc2006b3a62c5d9146356c5d5
Peter Kral <peter.kral@pdfix.net>
2025-11-05 10:31:42 +0100

    Adding config_file to main too

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
-DOCKER_IMAGE = "pdfix/generate-alternate-text-vision"
 CONFIG_FILE = "config.json"
+DOCKER_IMAGE = "pdfix/generate-alternate-text-vision"
 IMAGE_FILE_EXT_REGEX = r"\.(jpg|jpeg|png|bmp)$"
 SUPPORTED_IMAGE_EXT = ".jpg .jpeg .png .bmp"
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import traceback
 from pathlib import Path
 from typing import Any
 
-from constants import IMAGE_FILE_EXT_REGEX, SUPPORTED_IMAGE_EXT
+from constants import CONFIG_FILE, IMAGE_FILE_EXT_REGEX, SUPPORTED_IMAGE_EXT
 from exceptions import (
     EC_ARG_GENERAL,
     MESSAGE_ARG_GENERAL,
@@ -98,7 +98,7 @@ def get_pdfix_config(path: str) -> None:
     Args:
         path (string): Destination path for config.json file
     """
-    config_path = os.path.join(Path(__file__).parent.absolute(), "../config.json")
+    config_path: Path = Path(__file__).parent.parent.joinpath(CONFIG_FILE).resolve()
 
     with open(config_path, "r", encoding="utf-8") as file:
         if path is None:
